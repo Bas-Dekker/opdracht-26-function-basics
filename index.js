5f697b9b15a80df9ac1da505bddd7ea6c9a7c9bd
Bas <bmjdekker@gmail.com>
2021-03-05 13:40:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//Make Pizza
+
+const makePizza = function () {
+  console.log("Voeg water toe aan het bloem");
+  console.log("Kneed het deeg totdat het mooi egaal is");
+  console.log("Neem het deeg en rol het uit in een mooie, dunne cirkelvorm");
+};
+
+makePizza();
